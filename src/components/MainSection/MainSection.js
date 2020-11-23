40c21e39dd92971184d532f5009ae77f3b103f16
Jessica <jessikurt@gmail.com>
2020-11-23 08:17:47 +0000

    Add ImageSection

---
+++ src/components/MainSection/MainSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+import PictureFile from '../ImagesSection/PictureFile'
+
+
+function MainSection() {
+  return(
+    <Container> 
+      <PictureFile />
+    </Container>
+    );
+}
+
+export default MainSection;
