3da18aab809cb39e87767bee7f85297b17fa98a5
Jessica <jessikurt@gmail.com>
2020-11-23 08:18:25 +0000

    Included MainSection on GalleryPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import Header from '../components/Header'
+import Header from '../components/Header';
+import MainSection from '../components/MainSection';
 //import { Container } from './styles';
 
 function GalleryStorePage() {
   return(
     <div>
       <Header />
+      <MainSection />
     </div>
   );
 }
